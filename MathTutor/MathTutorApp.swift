cd06924a06c78d3804314ea688aaa693a684174a
Francisco2145 <fvaquerocampos@gmail.com>
2025-05-07 14:40:51 -0700

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Student2 on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
